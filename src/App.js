f84973ffe08a1dec8a1e2ad7af2ba3dc1c335db5
Jason Allen <jallend1@gmail.com>
2022-01-12 19:01:04 -0800

    Limits game to six guesses and adds letter by letter verification

--- src/App.js
+++ src/App.js
@@ -6,16 +6,38 @@ function App() {
   const words = ['howdy', 'score'];
   const word = words[1];
   const [guess, setGuess] = useState('');
+  const [guessHistory, setGuessHistory] = useState([]);
+  const [progress, setProgress] = useState([]);
   const [message, setMessage] = useState('');
 
   const checkGuess = (currentGuess) => {
-    console.log(guess);
-    console.log(currentGuess);
-    console.log(currentGuess === word);
     if (currentGuess === word) {
       setMessage('WE HAVE A WINNER');
+    } else {
+      // Array for tracking which letters are accurate
+      const answers = [];
+      const currentGuessArray = Array.from(currentGuess);
+      currentGuessArray.forEach((guess, index) => {
+        // If the letter is in the right spot, gets green
+        if (word[index] === guess) {
+          answers[index] = 'green';
+        }
+        // If the letter exists, but is in the wrong spot, gets yellow
+        else if (word.includes(guess)) {
+          answers[index] = 'yellow';
+        } else {
+          answers[index] = 'neutral';
+        }
+      });
+      const newGuessHistory = guessHistory.slice();
+      newGuessHistory.push(answers);
+      // You get six guesses, my dude
+      if (newGuessHistory.length === 6) {
+        setMessage('Game over :(');
+      }
+      setGuessHistory(newGuessHistory);
+      setGuess('');
     }
-    setGuess('');
   };
 
   const handleLetter = (e) => {
